f95548967832a0ff8145c06f21e2872e42c2ff29
Armen-Kazarov <ukraine097@ukr.net>
2018-02-05 00:28:25 +0200

    completed change background and stickers

--- src/components/sidemenu/stickers.js
+++ src/components/sidemenu/stickers.js
@@ -1,45 +1,36 @@
 import React from 'react';
+import '../../css/stickers.css'
 
 class Stickers extends React.Component {
     constructor() {
         super();
-
+        this.state = {
+            stickers: [
+                require('../../img/stickers/brown-om-nom.png'),
+                require('../../img/stickers/candy.png'),
+                require('../../img/stickers/chopper.png'),
+                require('../../img/stickers/cut-the-rope.png'),
+                require('../../img/stickers/om-nom.png'),
+                require('../../img/stickers/om-nom-friend.png'),
+                require('../../img/stickers/om-nom-happy.png'),
+                require('../../img/stickers/om-nom-in-headphones.png'),
+                require('../../img/stickers/om-nom-smile.png'),
+                require('../../img/stickers/on-nom-faro.png')
+            ],
+        };
     }
-
 
     render () {
         return (
             <div className="stickers-img">
-                <div>
-                    <img src="../../img/stickers/brown-om-nom.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/candy.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/chopper.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/cut-the-rope.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/om-nom.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/om-nom-friend.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/om-nom-happy.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/om-nom-in-headphones.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/om-nom-smile.png" alt="img"/>
-                </div>
-                <div>
-                    <img src="../../img/stickers/on-nom-faro.png" alt="img"/>
-                </div>
+                { this.state.stickers.map((sticker, index) => {
+                    return (
+                        <div key={index} className="stickers-block">
+                            <img src={sticker} alt="img" className="img-sticker"/>
+                        </div>
+                    )
+                })
+                }
             </div>
         )
     }
